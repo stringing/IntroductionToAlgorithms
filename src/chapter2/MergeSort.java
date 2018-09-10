c600480b976c16b5fed4f527fc3c6ae858a2dda5
Wenqiang Luo <943183575@qq.com>
2018-09-10 22:02:49 +0800

    无哨兵归并排序

--- src/chapter2/MergeSort.java
+++ src/chapter2/MergeSort.java
@@ -22,7 +22,8 @@ public class MergeSort {
             int mid = (int)Math.floor((start + end) >> 1);
             mergeSort(arr, start, mid);
             mergeSort(arr, mid + 1, end);
-            merge(arr, start, mid, end);
+            //merge(arr, start, mid, end);
+            mergeWithoutSentry(arr, start, mid, end);
         }
     }
 
@@ -42,6 +43,42 @@ public class MergeSort {
         int j = 0;
         for(int k = start; k <= end; k++){
             //当左数组右数组其中一个游标到了哨兵，由于任何数都会比max小，所以就会是没到哨兵的那个数组开始一直放数到arr中了
+            if(left[i] <= right[j]){
+                arr[k] = left[i];
+                i++;
+            }else{
+                arr[k] = right[j];
+                j++;
+            }
+        }
+    }
+
+    //无哨兵的实现
+    //ex2.3-2
+    private static void mergeWithoutSentry(int arr[], int start, int mid, int end){
+        int[] left = new int[mid - start + 1];
+        int[] right = new int[end - mid];
+        for(int x = start; x <= mid; x++){
+            left[x - start] = arr[x];
+        }
+        for(int y = mid + 1 ; y <= end; y++){
+            right[y - mid - 1] = arr[y];
+        }
+        int i = 0;
+        int j = 0;
+        for(int k = start; k <= end; k++){
+            if(i >= left.length){
+                for(int w = j; w < right.length; w++){
+                    arr[k++] = right[w];
+                }
+                break;
+            }
+            if(j >= right.length){
+                for(int w = i; w < left.length; w++){
+                    arr[k++] = left[w];
+                }
+                break;
+            }
             if(left[i] <= right[j]){
                 arr[k] = left[i];
                 i++;
